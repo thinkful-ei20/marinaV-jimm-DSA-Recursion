9e0629b01a0fa75092916cc68eb04dfaf84fd6dd
Marina Valiquette <marina.valiquette@gmail.com>
2018-06-18 16:26:35 -0400

    Add fibonacci drill

--- countingSheep.js
+++ countingSheep.js
@@ -22,4 +22,72 @@ const doubleInput = arr => {
 
 };
 
-console.log(doubleInput([1, 2, 3]));
+// console.log(doubleInput([1, 2, 3]));
+
+// Reverse string
+const reverseStr = str => {
+  // base case
+  if (!str.length) {
+    return "";
+  }
+  return reverseStr(str.substr(1)) + str.charAt(0);
+};
+
+// console.log(reverseStr('puzzle'));
+
+const calcTriangle = num => {
+  if (num === 0) {
+    return 0;
+  }
+
+  return num + calcTriangle(num - 1);
+};
+
+// console.log(calcTriangle(3));
+
+const strSplitter = (str, separator) => {
+  if(str.indexOf(separator) < 0) {
+    return [str];
+  }
+
+  return [str.substr(0, str.indexOf(separator)), ...strSplitter(str.substr(str.indexOf(separator) + 1), separator)];
+};
+
+console.log(strSplitter("Hello, World Clone, War", ","));
+
+const binaryNum = num => {
+  if(num < 2) {
+    return "1";
+  }
+
+  const binary = Math.floor(num % 2);
+
+  return binaryNum(Math.floor(num / 2)) + binary;
+};
+
+// console.log(binaryNum(25));
+
+const factorialNum = num => {
+  if (num === 1) {
+    return 1;
+  }
+
+  return num * factorialNum(num - 1);
+};
+
+// console.log(factorialNum(5));
+
+const findFibonacci = num => {
+  if (num === 1) {
+    return 1;
+  }
+
+  if (num === 2) {
+    return 1;
+  }
+
+  return findFibonacci(num - 1) + findFibonacci(num - 2);
+};
+
+console.log(findFibonacci(7));
+
