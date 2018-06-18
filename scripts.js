1ffb6957dfa78ea20d68415467c4a16e191bf89e
unknown <ajameslusk@gmail.com>
2018-06-18 15:41:27 -0400

    added factorial and fibonacci

--- scripts.js
+++ scripts.js
@@ -9,15 +9,14 @@ function countSheep(num){
 }
 // countSheep(3);
 
-// Array Double
-const doubleInput = arr => {
+const doubleArray = arr => {
   // base case
   if (!arr.length) {
     return [];
   }
   return [arr[0] * 2, ...doubleInput(arr.slice(1))];
 };
-//console.log(doubleInput([1, 2, 3]));
+// console.log(doubleArray([1, 2, 3]));
 
 const reverseString = str => {
   if(str.length === 1){
@@ -25,5 +24,75 @@ const reverseString = str => {
   }
   return [...reverseString(str.slice(-(str.length-1))), str[0]];
 };
+// console.log(reverseString('banana'));
 
-console.log(reverseString('banana').join(''));
+function nthTriangular(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n + nthTriangular(n - 1);
+}
+// console.log(nthTriangular(5));
+
+
+
+
+
+
+function stringSplitter(str, splitted){
+  if(str.length < 1){
+    return [];
+  }
+  if(str[0] === ','){
+    return [...stringSplitter(str.slice(1))];
+  }
+  return [str[0] + stringSplitter(str.slice(1))];
+}
+
+
+
+console.log(stringSplitter('hello,timm,im jimm', ','));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function factorial(n){
+  if(n === 0){
+    return 1;
+  }
+  return n*factorial(n-1);
+}
+// console.log(factorial(5));
+
+function fibonacci(n){
+  if(n === 1){
+    return 1;
+  }
+  if(n === 2){
+    return 1;
+  }
+  return fibonacci(n-1)+fibonacci(n-2);
+}
+// console.log(fibonacci(7));
+
+/*
+  e   a   s   t
+  ea  ae  se
+  es  as  sa
+  et  at  st
+*/
+function anagrams(str){
+
+}
+// anagrams('east');
